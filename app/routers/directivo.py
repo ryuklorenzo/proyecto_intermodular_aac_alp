00eefb9866f591a2abed7447e6d0b47affa49da0
ryuk_Lorenzo <angel14lpl@gmail.com>
2026-01-14 17:26:24 +0100

    Router de directivo creado.

--- app/routers/directivo.py
+++ app/routers/directivo.py
@@ -1,22 +1,21 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from typing import List
-from app.models import ProfesorCreate, ProfesorDb, ProfesorBase
+from app.models import DirectivoCreate, DirectivoDb, DirectivoBase
 from app.auth.auth import oauth2_scheme, TokenData # Si quieres proteger las rutas con token
-from app.database import insert_profesor,delete_profesor, read_all_profesores,read_profesor_by_id,  profesor_exists, validateIsAdmin
+from app.database import insert_directivo, read_directivo_by_id, delete_directivo,  read_all_directivos, directivo_exists, validateIsAdmin
 
 router = APIRouter(
-    prefix="/techers",
-    tags=["Teachers"]
+    prefix="/executives",
+    tags=["Executives"]
 )
 
-# 1. Insertar Profesors
 @router.post("/create", status_code=status.HTTP_201_CREATED, response_model=dict)
-async def crear_profesor(profesor: ProfesorCreate, token: str = Depends(oauth2_scheme)):
+async def crear_directivo(directivo: DirectivoCreate, token: str = Depends(oauth2_scheme)):
     if validateIsAdmin(token) == True:
         try: 
-            if profesor_exists(profesor.nombre, profesor.apellidos) == False:
-                profesor_id = insert_profesor(profesor)
-                return {"message": "Profesor creado exitosamente", "id": profesor_id}
+            if directivo_exists(directivo.nombre, directivo.apellidos, directivo.cargo) == False:
+                directivo_id = insert_directivo(directivo)
+                return {"message": "directivo creado exitosamente", "id": directivo_id}
             else: 
                 raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -25,7 +24,7 @@ async def crear_profesor(profesor: ProfesorCreate, token: str = Depends(oauth2_s
         except Exception as e:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=f"Error, ese profesor ya esta creado"
+                detail=f"Error, ese directivo ya esta creado"
             )
     else:
         raise HTTPException(
@@ -34,13 +33,12 @@ async def crear_profesor(profesor: ProfesorCreate, token: str = Depends(oauth2_s
             )
 
 
-# 2. Ver todos los PROFESORES
-@router.get("/", response_model=List[ProfesorDb], status_code=status.HTTP_200_OK)
-async def ver_profesores(token: str = Depends(oauth2_scheme)):
+@router.get("/", response_model=List[DirectivoDb], status_code=status.HTTP_200_OK)
+async def ver_directivo(token: str = Depends(oauth2_scheme)):
     if validateIsAdmin(token) == True:
     # Aquí podrías añadir Depends(oauth2_scheme) si quieres que solo usuarios logueados lo vean 
-        profesores = read_all_profesores()
-        return profesores
+        directivo = read_all_directivos()
+        return directivo
     else:
             raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED,
@@ -48,16 +46,16 @@ async def ver_profesores(token: str = Depends(oauth2_scheme)):
                 )
 
 
-@router.get("/{id}/", response_model=ProfesorCreate, status_code=status.HTTP_200_OK)
-async def ver_profesor_por_id(id: int, token: str = Depends(oauth2_scheme)):
+@router.get("/{id}/", response_model=DirectivoCreate, status_code=status.HTTP_200_OK)
+async def ver_directivo_por_id(id: int, token: str = Depends(oauth2_scheme)):
     if validateIsAdmin(token) == True:
-        profesor = read_profesor_by_id(id)
-        if not profesor:
+        directivo = read_directivo_by_id(id)
+        if not directivo:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Profesor con id {id} no encontrado"
+                detail=f"Directivo con id {id} no encontrado"
             )
-        return profesor
+        return directivo
     else:
             raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED,
@@ -66,15 +64,15 @@ async def ver_profesor_por_id(id: int, token: str = Depends(oauth2_scheme)):
 
 
 @router.delete("/{id}/", status_code=status.HTTP_200_OK)
-async def delete_user(profesorBase : ProfesorBase, token: str = Depends(oauth2_scheme)):
+async def delete_directivo(directivoBase : DirectivoBase, token: str = Depends(oauth2_scheme)):
     if validateIsAdmin(token) == True:
-        deleted = delete_profesor(profesorBase)
+        deleted = delete_directivo(directivoBase)
         if not deleted:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, # O 404
-                detail="Error: Profesor no encontrado o contraseña incorrecta"
+                detail="Error: Directivo no encontrado o contraseña incorrecta"
             )
-        return {"message": "Profesor eliminado correctamente"}
+        return {"message": "Directivo eliminado correctamente"}
     else:
             raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED,
