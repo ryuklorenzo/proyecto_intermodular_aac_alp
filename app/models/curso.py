4fe83391090f164a541de4d54dc282e1d31e7394
ryuk_Lorenzo <angel14lpl@gmail.com>
2026-05-12 20:47:42 +0200

    FEAT: cursoout con sus campos

--- app/models/curso.py
+++ app/models/curso.py
@@ -6,5 +6,11 @@ class CursoCreate(BaseModel):
     modulo: str
     id_horario: int
 
-class CursoOut(CursoCreate):
+class CursoOut(BaseModel):
     id:int
+    nivel: str
+    curso: str
+    modulo: str
+    formato: str
+    hora_inicio: str
+    hora_fin: str
