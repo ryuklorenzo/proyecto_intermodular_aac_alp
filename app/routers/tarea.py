a1ca72929182dade4e4bc5a1dda5a7caaf6884d8
ryuk_Lorenzo <angel14lpl@gmail.com>
2026-05-14 23:24:38 +0200

    FIX: crear tarea actualizado, funciones agreagdas.

--- app/routers/tarea.py
+++ app/routers/tarea.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from typing import List
-from app.models.tarea import TareaCreate, TareaOut
+from app.models.tarea import TareaBase, TareaCreate, TareaOut
 from app.auth.auth import oauth2_scheme
 from app.database.tarea import (
     insert_tarea, 
     read_tareas_by_alumno, 
     read_tareas_by_profesor
 )
+from app.database.alumno import read_alumno_by_id
+from app.database.profesor import read_profesor_by_id
 from app.database.user import read_user_by_id
 from app.database.database_config import validateIsAdmin
 
@@ -17,18 +19,26 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def crear_tarea(
-    tarea: TareaCreate,
-    token: str = Depends(oauth2_scheme)
+    id_profesor: int,
+    id_alumno: int,
+    tarea: TareaBase,
+    # token: str = Depends(oauth2_scheme)
 ):
-    if not validateIsAdmin(token):
-        raise HTTPException(status_code=401, detail="UNAUTHORIZED")
+    # if not validateIsAdmin(token):
+    #     raise HTTPException(status_code=401, detail="UNAUTHORIZED")
+    if not read_alumno_by_id(id_alumno):
+        raise HTTPException(status_code=404, detail="Alumno no encontrado")
+
+    if not read_profesor_by_id(id_profesor):
+        raise HTTPException(status_code=404, detail="Profesor no encontrado")
+
     new_tarea = TareaCreate(
         descripcion=tarea.descripcion,
         estado=tarea.estado,
-        id_profesor=tarea.id_profesor,
-        id_alumno=tarea.id_alumno
+        id_profesor=id_profesor,
+        id_alumno=id_alumno
     )
-    tarea_id = insert_tarea(tarea)
+    tarea_id = insert_tarea(new_tarea)
 
     if tarea_id == -1:
         raise HTTPException(status_code=500, detail="Error al crear la tarea")
@@ -36,14 +46,22 @@ async def crear_tarea(
     return {"message": "Tarea asignada correctamente", "id": tarea_id}
 
 @router.get("/students/{id_alumno}/", response_model=List[TareaOut])
-async def ver_tareas_alumno(id_alumno: int, token: str = Depends(oauth2_scheme)):
-    
+async def ver_tareas_alumno(
+    id_alumno: int, 
+    # token: str = Depends(oauth2_scheme)
+):
+    # if not validateIsAdmin(token):
+    #     raise HTTPException(status_code=401, detail="UNAUTHORIZED")
     tareas = read_tareas_by_alumno(id_alumno)
     return tareas
 
 
 @router.get("/teachers/{id_profesor}/", response_model=List[TareaOut])
-async def ver_tareas_profesor(id_profesor: int, token: str = Depends(oauth2_scheme)):
-    
+async def ver_tareas_profesor(
+    id_profesor: int, 
+    # token: str = Depends(oauth2_scheme)
+):
+    # if not validateIsAdmin(token):
+    #     raise HTTPException(status_code=401, detail="UNAUTHORIZED")
     tareas = read_tareas_by_profesor(id_profesor)
     return tareas
