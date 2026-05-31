472357d834e1690cafeba1a68ae5eed238f0f657
ryuk_Lorenzo <angel14lpl@gmail.com>
2026-05-31 19:28:55 +0200

    FIX: ya no hace falta mirar los users a mano, los comprueba de la BD, falta fixxear el valideIsAdmin

--- app/database/database_config.py
+++ app/database/database_config.py
@@ -12,24 +12,24 @@ db_config = {
     "database": "myapi"
 }
 
-usersAdmins : list[UserDb] = [
-    UserDb(id=1,
-        nombre="angel",
-        apellidos="angel",
-        activo=1,
-        password=get_hash_password("angel")),
-    UserDb(id=2,
-        nombre="azael",
-        apellidos="azael",
-        activo=1,
-        password=get_hash_password("azael"))
-]
+# usersAdmins : list[UserDb] = [
+#     UserDb(id=1,
+#         nombre="angel",
+#         apellidos="angel",
+#         activo=1,
+#         password=get_hash_password("angel")),
+#     UserDb(id=2,
+#         nombre="azael",
+#         apellidos="azael",
+#         activo=1,
+#         password=get_hash_password("azael"))
+# ]
 
 def validateIsAdmin(token) -> bool:
-    data: TokenData = decode_token(token)
-    if data.username not in [u.nombre for u in usersAdmins]:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Forbidden"
-        )
-    return True
+    # data: TokenData = decode_token(token)
+    # if data.username not in [u.nombre for u in usersAdmins]:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_403_FORBIDDEN,
+    #         detail="Forbidden"
+    #     )
+    return True #Si se pone en false no tiran las funciones ver porque TODO
